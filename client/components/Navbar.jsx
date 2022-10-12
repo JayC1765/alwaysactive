a787dced0ac617880418c415a2ecf49e9cdcfc8f
William Ku <william.ku9192@gmail.com>
2022-10-12 19:39:50 -0400

    added logo to navbar and set it to display flex

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../containers/Logo'
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <Logo />
       <ul>
         <li><Link to="/signuplogin" state={true}>Sign Up</Link></li>
         <li><Link to="/signuplogin" state={false}>Log In</Link></li>
