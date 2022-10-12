c8e2623d2630dbc6e95f2967d55694e79a6dffd9
JayC1765 <Jason.chann91@gmail.com>
2022-10-12 14:47:06 -0400

    cleanup

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -30,31 +30,4 @@ function SignUp(props) {
   );
 }
 
-// function SignUp(props) {
-//   return (
-//     <div>
-//       <h1>Sign Up</h1>
-//       <form>
-//         <input id='inputBox'
-//           onChange={(e) => {props.updateSignUpUsername(e.target.value)}}
-//           value={props.signUpUsername}
-//           placeholder='Username'
-//           type='text'
-//         />
-//         <input id='inputBox'
-//           onChange={(e) => {props.updateSignUpPassword(e.target.value)}}
-//           value={props.signUpPassword}
-//           placeholder='Password'
-//           type='text'
-//         />
-//         <div>
-//           <button id='signUpBtn' onClick={() => {props.saveUser()}}>
-//             Sign Up
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   )
-// };
-
 export default SignUp;
