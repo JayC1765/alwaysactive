3e9ad9e59ceaa14bdd8f9e7562a5fe057d8399b7
JayC1765 <Jason.chann91@gmail.com>
2023-01-02 17:57:20 -0500

    updated server scripts for TypeScript

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,14 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
+      // {
+      //   test: /\.(ts|js)x?$/,
+      //   exclude: /node_modules/,
+      //   loader: 'babel-loader',
+      //   options: {
+      //     presets: ['@babel/preset-env', '@babel/preset-react'],
+      //   },
+      // },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
